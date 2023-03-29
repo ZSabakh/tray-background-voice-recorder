306b63f560914586dc1dea82957786cdf881c9b2
ZSabakh <zsabakhtarishvili@atu.edu>
2023-03-28 23:01:04 -0500

    Added options to set desired interval for recordings

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import lameenc
 import pyaudio
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QMainWindow, QTextEdit
+from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QMainWindow, QTextEdit, QDialog, QVBoxLayout, QLabel, \
+    QLineEdit, QButtonGroup, QRadioButton, QDialogButtonBox, QMessageBox, QPushButton, QComboBox
 
 
 class TrayIcon(QSystemTrayIcon):
     def __init__(self, icon, parent=None):
         QSystemTrayIcon.__init__(self, icon, parent)
+        self.interval = 5
         self.textbox = None
         self.window = None
         self.recordingState = False
@@ -24,6 +26,7 @@ class TrayIcon(QSystemTrayIcon):
         stopRecording = menu.addAction("Stop Recording")
         showFileLocation = menu.addAction("Show File Location")
         readMe = menu.addAction("Read Me")
+        options = menu.addMenu("Options")
         exitAction = menu.addAction("Exit")
         self.setContextMenu(menu)
 
@@ -32,16 +35,64 @@ class TrayIcon(QSystemTrayIcon):
         showFileLocation.triggered.connect(self.showFileLocation)
         exitAction.triggered.connect(self.exitAction)
         readMe.triggered.connect(self.readMe)
+        options.addAction("Set Interval")
+
+        options.triggered.connect(self.optionSelected)
+
+    def optionSelected(self, action):
+        if action.text() == "Set Interval":
+            interval_dialog = QDialog()
+            interval_dialog.setWindowTitle("Set Interval")
+
+            layout = QVBoxLayout()
+
+            instructions_label = QLabel("Enter a number from 0-60:")
+            layout.addWidget(instructions_label)
+
+            interval_input = QLineEdit()
+            layout.addWidget(interval_input)
+
+            time_units_selector = QComboBox()
+            time_units_selector.addItem("Seconds")
+            time_units_selector.addItem("Minutes")
+            layout.addWidget(time_units_selector)
+
+            submit_button = QPushButton("Submit")
+            layout.addWidget(submit_button)
+
+            submit_button.clicked.connect(
+                lambda: self.handle_interval_input(interval_input, time_units_selector, interval_dialog))
+
+            interval_dialog.setLayout(layout)
+            interval_dialog.exec()
+
+    def handle_interval_input(self, interval_input, time_units_selector, interval_dialog):
+        try:
+            interval = int(interval_input.text())
+
+            if 0 <= interval <= 60:
+                if time_units_selector.currentText() == "Minutes":
+                    interval *= 60
+
+                self.interval = interval
+                interval_dialog.accept()
+            else:
+                QMessageBox.warning(interval_dialog, "Error", "Please enter a number from 0-60.")
+        except ValueError:
+            QMessageBox.warning(interval_dialog, "Error", "Please enter a valid integer.")
+
 
     def showFileLocation(self):
         current_location = os.path.dirname(os.path.abspath(__file__))
-        print("Current location:", current_location)
         if sys.platform == 'win32':
             subprocess.Popen(f'explorer "{current_location}"')
-        elif sys.platform.startswith('linux'):
+        elif sys.platform == 'darwin':
+            subprocess.Popen(['open', current_location])
+        elif sys.platform == 'linux':
             subprocess.Popen(['xdg-open', current_location])
         else:
             print("Unsupported platform")
+
 
     def readMe(self):
         self.window = QMainWindow()
@@ -75,11 +126,10 @@ class TrayIcon(QSystemTrayIcon):
     def recording(self):
         audio = pyaudio.PyAudio()
         stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-        interval = 5  # seconds
         frames = []
 
         while self.recordingState:
-            for i in range(0, int(44100 / 1024 * interval)):
+            for i in range(0, int(44100 / 1024 * self.interval)):
                 data = stream.read(1024)
                 frames.append(data)
             # save the audio
@@ -113,6 +163,7 @@ class TrayIcon(QSystemTrayIcon):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
     w = TrayIcon(QIcon("green.png"))
     w.show()
     sys.exit(app.exec())
